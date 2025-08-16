6aeaead2c5ef026128445c25f295f6f64db4e797
flipping-v3nge <talisj3ff@gmail.com>
2025-08-16 17:53:14 -0400

    Create NewsSignalService.java

---
+++ src/main/java/com/yourname/marketadvisor/NewsSignalService.java
@@ -0,0 +1,12 @@
+package com.yourname.marketadvisor;
+
+import java.util.*;
+
+class NewsSignalService
+{
+    List<String> fetchInsights(String rssUrl)
+    {
+        // Placeholder: would parse OSRS news RSS
+        return Arrays.asList("Upcoming quest", "New boss release");
+    }
+}
